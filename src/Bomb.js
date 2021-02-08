3e6f330db997dff1ce3e8978cac7d2c3226963a3
yoelyukalo <yoelyukalo@gmail.com>
2021-02-07 23:28:48 -0600

    commit tree 4bcb18fada3f7dca1530bea10e7d8fc220fdfb7f

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,13 @@
-// your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+
+    render(){
+        return(
+            <h1>done1</h1>
+        )
+    }
+
+}
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,14 @@
-// your ImageSlider code here!
+import React from 'react';
+
+class ImageSlider extends React.Component {
+
+    render(){
+
+        return(
+            <h1>done2</h1>
+        )
+    }
+
+}
+
+export default ImageSlider;
